848b51c326f84408e4ca4833ffa0ad57b8a4290e
citymer <cityhunter8484@gmail.com>
2022-05-27 13:44:46 +0200

    ajout de "expect" dans test_Bill.js

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -25,8 +25,8 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills)
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
-
+      //on verifie si windowIcon a cette classe
+      expect(windowIcon.classList.contains("active-icon")).toBe(true)
     })
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
