5ebb96b94f1b28cb129580bbd7bfac1c6d989b83
citymer <cityhunter8484@gmail.com>
2022-06-09 10:15:43 +0200

    test affichage du formulaire dans newBill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,31 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import { screen } from '@testing-library/dom'
+import NewBillUI from '../views/NewBillUI.js'
+import NewBill from '../containers/NewBill.js'
+import { localStorageMock } from '../__mocks__/localStorage'
+import router from '../app/Router'
+import { ROUTES_PATH } from '../constants/routes.js'
 
-
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+describe('Given I am connected as an employee', () => {
+  describe('When I am on NewBill Page', () => {
+    test('Then the newBill page should be rendered', () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      //to-do write assertion
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem(
+        'user',
+        JSON.stringify({
+          type: 'Employee',
+        })
+      )
+      const root = document.createElement('div')
+      root.setAttribute('id', 'root')
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      expect(screen.getAllByText('Envoyer une note de frais')).toBeTruthy()
     })
   })
 })
